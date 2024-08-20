3c2fb8d5432f08c6ae982931e203b72d8d4c259b
Daniel Moessner <dmoessne@redhat.com>
2024-08-20 15:14:44 +0200

    overhoul

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ A web server providing access to the packages to be installed on top of an exist
 How to use this:
  - a registry to push the images to (in this example quay)
  - put needed packages on a web server the system running the build has access to 
- - adjust the Containerfile accordingly (web server address, packages, additional packages may be needed, check errors in build process)
- - adijust the create script to refelct for which RHCOS version you want to build the imagei(s), the path to the registry secret with write permissions and the path in the registry (must exist)
- - after having this done, kick of the create script (see help to choose just building, pushing or both)
+ - adjust the `Containerfile` accordingly (web server address, packages, additional packages may be needed, check errors in build process)
+ - adijust the variables at the beginning of `create-layered-rhcos.sh`, e.g. 
+   - RHCOS version you want to build the image(s) (`USED_TAGS=`, `MAJOR=`, `MINOR=`)
+     - `USED_TAGS=` is used as the z-stream you want to build (see your current cluster version) and can be an array to build and push for multiple z-streams
+   - your registry Destination (`QUAY_DEST=`) and login secret (`POD_LOGIN=`) to push the created image(s) to 
+   - additional tag to add (`QUAY_TAG=`
+ - after having this done, kick of the create script (see help to choose just building (`b`), pushing (`p`)or both (`a`))
+
--- create-layered-rhcos.sh
+++ create-layered-rhcos.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 # VARS
 ## set tags to be build
-USED_TAGS=(21 22 23 24 25) 
-#USED_TAGS=(25)
+#USED_TAGS=(21 22 23 24 25) 
+USED_TAGS=(5 6)
 ## set major and minor version to be used
 MAJOR=4
-MINOR=14
+MINOR=16
 ## set destination in quay to push
-QUAY_DEST=quay.io/dmoessne/test123
+QUAY_DEST=quay.io/dmoessne/rhel-46814-bisect
+QUAY_TAG=8
 ## set name of the Containerfile
 CONT_FILE=Containerfile
 ## set location of podman login file for private repo
-POD_LOGIN="/home/dm/.docker/dmoessne-quay-auth.json"
+POD_LOGIN="/home/dmoessne/.config/containers/dmoessne-dmoessne-quay-auth.json"
 ## VARS END
 
 build () {
    for TAG in "${USED_TAGS[@]}";do
        echo -n "${TAG} : "
-       RHCOS_VER=`oc adm release info quay.io/openshift-release-dev/ocp-release:${MAJOR}.${MINOR}.${TAG}-x86_64 --image-for=rhel-coreos` >&/dev/null
-       podman build --network host -t ${QUAY_DEST}:${MAJOR}.${MINOR}.${TAG} --build-arg RHCOS_VER=${RHCOS_VER}  -f ${CONT_FILE} >&/dev/null 
+       RHCOS_VER=`oc adm release info quay.io/openshift-release-dev/ocp-release:${MAJOR}.${MINOR}.${TAG}-x86_64 --image-for=rhel-coreos` #>&/dev/null
+       podman build --network host -t ${QUAY_DEST}:${MAJOR}.${MINOR}.${TAG}-${QUAY_TAG} --build-arg RHCOS_VER=${RHCOS_VER}  -f ${CONT_FILE} #>&/dev/null 
        if [ $? -ne 0 ]
        then
          echo "something went wrong with build of ${TAG}"
@@ -32,7 +33,7 @@ build () {
 push () {
    for TAG in "${USED_TAGS[@]}";do
        echo "     pushing image"
-       podman push --authfile=${POD_LOGIN} ${QUAY_DEST}:${MAJOR}.${MINOR}.${TAG} >&/dev/null
+       podman push --authfile=${POD_LOGIN} ${QUAY_DEST}:${MAJOR}.${MINOR}.${TAG}-${QUAY_TAG} #>&/dev/null
        if [ $? -ne 0 ]
        then
          echo "     something went wrong with pushing of ${TAG} to quay"
